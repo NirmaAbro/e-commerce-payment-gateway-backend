78f88a4cf121431d7b77de1b87baa8972695234b
Nirma Abro <NirmaAbro143@gmail.com>
2024-11-30 15:57:47 -0600

    payment-gateway-added

--- Controllers/PaymentController.js
+++ Controllers/PaymentController.js
@@ -3,15 +3,17 @@ const stripe = require("../config/stripeConfig");
 
 exports.createCheckoutSession = async (req, res) => {
   const { products } = req.body;
-  console.log("products", products)
+  console.log("products", products);
 
   const lineItems = products.map((product) => ({
     price_data: {
       currency: "usd",
       product_data: {
-        name: product.name,
+        name: product.title,
+        description: product.description,
+        images: [product.image],
       },
-      unit_amount: product.price * 100, // Convert price to cents
+      unit_amount: Math.round(product.price * 100), // Convert price to cents and round to avoid floating-point issues
     },
     quantity: product.qty,
   }));
@@ -21,17 +23,16 @@ exports.createCheckoutSession = async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.CLIENT_URL}/success`, // Success page
+      success_url:  `${process.env.CLIENT_URL}/success`, // Success page
       cancel_url: `${process.env.CLIENT_URL}/cancel`,  // Cancel page
     });
 
-     // Log the created session
-     console.log("Session created:", session);
+    // Log the created session
+    console.log("Session created:", session);
 
-     
     res.status(200).json({ id: session.id });
   } catch (error) {
     console.error("Error creating checkout session:", error.message);
     res.status(500).json({ error: "Failed to create checkout session" });
   }
-};
+}
